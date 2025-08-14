84ed76bf1c9352e22f908809ca32d948a4544e73
katabaazi <kimkatabazi@gmail.com>
2025-08-15 00:21:50 +0300

    Added Fahrenheit to Celsius conversion

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from covernter import celsius_to_fahrenheit
+from covernter import celsius_to_fahrenheit, fahrenheit_to_celsius
 print("Temperature Converter")
 
+print("1. celsius to fahrenheit")
+print("2. fahrenheit to celsius")
 
-try:
-    celsius = float(input("Enter a temperture in Celsius: "))
+choice = input("Choose a conversion (1 or 2)")
 
-    fahrenheit = celsius_to_fahrenheit(celsius)
+if choice == "1":
 
-    print(f"{celsius}C is equal to {fahrenheit}F")
+    try:
+        celsius = float(input("Enter a temperture in Celsius: "))
 
-except ValueError:
-    print("invalid input. Please enter a number.")
+        fahrenheit = celsius_to_fahrenheit(celsius)
+
+        print(f"{celsius}C is equal to {fahrenheit}F")
+
+    except ValueError:
+        print("invalid input. Please enter a number.")
+
+elif choice == "2":
+    try:
+        fahrenheit = float(input("Enter a temperture in fahrenheit: "))
+
+        celsius = fahrenheit_to_celsius(fahrenheit)
+
+        print(f"{fahrenheit}F is equal to {celsius}C")
+
+    except ValueError:
+        print("invalid input. Please enter a number.")
+
+else:
+    print("Invalid choice")
